be0abfee0693547a02d14e26261213a95fb87cc0
kremlevmax <kremlevmax1989@gmail.com>
2022-10-04 15:50:56 -0700

    Connect to

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const connectToDB = require("./db/connect");
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on a port ${PORT}...`);
-});
+const start = () => {
+  connectToDB(process.env.MONGO_URI);
+  app.listen(PORT, () => {
+    console.log(`Server is listening on a port ${PORT}...`);
+  });
+};
+
+start();
